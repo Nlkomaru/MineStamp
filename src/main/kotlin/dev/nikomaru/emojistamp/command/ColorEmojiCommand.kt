9c709fb87d730ec2d4b278a3fa60968916cc84fe
Nlkomaru <nikomaru@nikomaru.dev>
2023-07-21 18:07:28 +0900

    Process work in async thread.

--- src/main/kotlin/dev/nikomaru/emojistamp/command/ColorEmojiCommand.kt
+++ src/main/kotlin/dev/nikomaru/emojistamp/command/ColorEmojiCommand.kt
@@ -7,23 +7,16 @@ import cloud.commandframework.annotations.suggestions.Suggestions
 import cloud.commandframework.context.CommandContext
 import com.comphenix.protocol.PacketType
 import com.comphenix.protocol.ProtocolLibrary
-import com.comphenix.protocol.events.PacketContainer
-import com.comphenix.protocol.wrappers.EnumWrappers
 import com.comphenix.protocol.wrappers.WrappedParticle
-import com.destroystokyo.paper.ParticleBuilder
 import dev.nikomaru.emojistamp.EmojiStamp
-import dev.nikomaru.emojistamp.utils.coroutines.async
 import dev.nikomaru.emojistamp.utils.coroutines.minecraft
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
-import org.bukkit.Bukkit
 import org.bukkit.Color
-import org.bukkit.Location
 import org.bukkit.Particle
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
-import org.bukkit.inventory.meta.trim.TrimMaterial.REDSTONE
 import javax.imageio.ImageIO
 import kotlin.math.cos
 import kotlin.math.sin
@@ -38,9 +31,9 @@ class ColorEmojiCommand {
         @javax.annotation.Nullable @Range(min = "1", max = "10") @Argument(value = "time") inputTime: Int?,
         @javax.annotation.Nullable @Range(min = "1", max = "100") @Argument(value = "size") inputSize: Int?,
         @javax.annotation.Nullable @Range(
-            min = "1",
-            max = "500"
-        ) @Argument(value = "particleSize") inputParticleSize: Int?
+            min = "0.01",
+            max = "4"
+        ) @Argument(value = "particleSize") inputParticleSize: Float?
     ) {
         if (sender !is Player) {
             sender.sendPlainMessage("プレイヤーから実行してください")
@@ -53,17 +46,25 @@ class ColorEmojiCommand {
 
         val time = inputTime ?: 1
         val size = inputSize ?: 1
+        val particleSize = inputParticleSize ?: 0.5f
 
         val location = sender.location
+
+        val locX = location.x
+        val locY = location.y
+        val locZ = location.z
+        val locYaw = location.yaw
 
         val unicode = EmojiStamp.emojiProperties.getProperty(cldr).split(" ").joinToString("_") { it.lowercase() }
         val pictureName = "emoji_u${unicode}.png"
         val image = ImageIO.read(this.javaClass.classLoader.getResourceAsStream("noto-emoji_128/$pictureName"))
 
+        val decrease = 4
+
         val list = arrayListOf<Pair<Pair<Int, Int>, Int>>()
         repeat(image.width) { x ->
             repeat(image.height) { y ->
-                if (image.getRGB(x, y) != 0) {
+                if (image.getRGB(x, y) != 0 && x % decrease == 0 && y % decrease == 0) {
                     list.add(Pair(Pair(x, y), image.getRGB(x, y)))
                 }
             }
@@ -84,31 +85,28 @@ class ColorEmojiCommand {
             location.getNearbyPlayers(20.0)
         }
 
-        withContext(Dispatchers.async){
-            repeat(20 * time) {
-                list.forEach {
+        val count = 8
+
+        withContext(Dispatchers.Default) {
+            repeat(count * time) {
+                list.parallelStream().forEach {
                     val position = it.first
                     val x = (position.first - (xMin + midWidth)) / width * 3 * size // 80 / 160 * 3 = 1.5
                     val y = (yMax - position.second) / height * 3 * size
-                    val particleLocation = Location(
-                        location.world,
-                        location.x + (x * cos(-location.yaw.toDouble() / 180 * Math.PI)),
-                        location.y + y + 2,
-                        location.z + (x * sin(location.yaw.toDouble() / 180 * Math.PI))
-                    )
                     val packet = pm.createPacket(PacketType.Play.Server.WORLD_PARTICLES)
                     packet.newParticles.write(
                         0,
-                        WrappedParticle.create(Particle.REDSTONE, Particle.DustOptions(Color.fromARGB(it.second), 0.5f))
+                        WrappedParticle.create(Particle.REDSTONE, Particle.DustOptions(Color.fromARGB(it.second), particleSize))
                     )
-                    packet.doubles.write(0, particleLocation.x)
-                        .write(1, particleLocation.y)
-                        .write(2, particleLocation.z)
+                    packet.doubles.write(0, locX + (x * cos(-locYaw.toDouble() / 180 * Math.PI)))
+                        .write(1, locY + y + 2)
+                        .write(2, locZ + (x * sin(locYaw.toDouble() / 180 * Math.PI)))
+
                     players.forEach { player ->
                         pm.sendServerPacket(player, packet)
                     }
                 }
-                delay(50L)
+                delay(1000L / count)
             }
         }
     }
